2f20995b7f702c2ff9b6bdd0069e63782f98f537
MrOnion123 <doquanghuy031105@gmail.com>
2026-05-10 00:03:31 +0700

    move sql to socialnet

--- socialnet/db.php
+++ socialnet/db.php
@@ -1,5 +1,5 @@
 <?php
-$conn = new mysqli("localhost", "your-username", "your-password", "database-name");
+$conn = new mysqli("localhost", "huy", "Quanghuy031105@", "socialnet");
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
